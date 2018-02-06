0f9ff91b850507e2b425fc523434797f8b4fc3aa
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2018-02-06 22:35:06 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# sites
+# Sites
+
+Adding some default sites:
+
+* Add an array of JS objects which looks like this:
+  ```json
+  {
+    name: 'SITE-NAME',
+    url: 'SITE-URL'
+  }
+  ```
+
+  To the object **defaultSites** in the file `script.js`.
---
+++ script.js
@@ -0,0 +1,55 @@
+const loadSitesToList = sites => {
+  const listElement = document.querySelector('ul#sites');
+
+  listElement.innerHTML = sites.map(site => `<li><a href="${site.url}" target="_blank">${site.name}</a></li>`).join('');
+};
+
+const openDefaultLinks = () => {
+  const button = document.querySelector('#go-to-sites');
+
+  //Add here the array of JS objects sites
+  const defaultSites = [];
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  loadSitesToList(defaultSites);
+
+  button.addEventListener('click', () => {
+    const radioButtonSecretedValue = Array.from(document.querySelectorAll('input[name="choose-links"]')).filter(input => input.checked)[0].value;
+    const textareaValue = document.querySelector('#custom-links-textarea').value;
+    const customLinks = textareaValue ? textareaValue.replace(/(http.*)\s*/g, '$1').split(',') : null;
+    let allSites;
+
+    switch (radioButtonSecretedValue) {
+      case 'only-default-sites': {
+        allSites = defaultSites.map(site => site.url);
+        break;
+      }
+      case 'only-custom-links': {
+        allSites = customLinks ? customLinks : [];
+        if (!customLinks) {
+          alert('You need to enter values to the box!');
+        }
+        break;
+      }
+      case 'both': {
+        allSites = customLinks ? defaultSites.map(site => site.url).concat(customLinks) : defaultSites.map(site => site.url);
+        break;
+      }
+      default: {
+        allSites = defaultSites.map(site => site.url);
+        break;
+      }
+    }
+
+    for (let site of allSites) {
+      window.open(site, '_blank');
+    }
+  });
+};
+
+// run the code when DOM is fully loaded.
+if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
+  openDefaultLinks();
+} else {
+  document.addEventListener('DOMContentLoaded', openDefaultLinks);
+}
