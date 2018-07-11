952fb6a75fd9c76dc894a163239c8ae04cb0f692
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2018-07-11 23:58:09 +0300

    added a button to insert all open tabs to the textarea

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 /* global chrome */
 
-// TODO: Add an option to copy all sites urls to string.
 // TODO: add a check not to open index.html when already opened.
 // TODO: Add an option to set the data on the site it self (at the moment as prompt dialog) and save it with local storage.
 // TODO: Add icons.
 // TODO: change the page to a React APP.
 // TODO: Add an option to add/remove a site.
+
+// Add here the array of JS objects sites
+const defaultSites = [];
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 const loadSitesToList = sites => {
   const listElement = document.querySelector('ul#sites');
@@ -13,12 +16,8 @@ const loadSitesToList = sites => {
   listElement.innerHTML = sites.map(site => `<li><a href="${site.url}" target="_blank">${site.name}</a></li>`).join('');
 };
 
-const openDefaultLinks = () => {
+const setOpenDefaultLinks = () => {
   const button = document.querySelector('#go-to-sites');
-
-  // Add here the array of JS objects sites
-  const defaultSites = [];
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   loadSitesToList(defaultSites);
 
@@ -29,29 +28,29 @@ const openDefaultLinks = () => {
     let allSites;
 
     switch (radioButtonSecretedValue) {
-    case 'only-default-sites':
-    {
-      allSites = defaultSites.map(site => site.url);
-      break;
-    }
-    case 'only-custom-links':
-    {
-      allSites = customLinks || [];
-      if (!customLinks) {
-        alert('You need to enter values to the box!');
-      }
-      break;
-    }
-    case 'both':
-    {
-      allSites = customLinks ? defaultSites.map(site => site.url).concat(customLinks) : defaultSites.map(site => site.url);
-      break;
-    }
-    default:
-    {
-      allSites = defaultSites.map(site => site.url);
-      break;
-    }
+      case 'only-default-sites':
+        {
+          allSites = defaultSites.map(site => site.url);
+          break;
+        }
+      case 'only-custom-links':
+        {
+          allSites = customLinks || [];
+          if (!customLinks) {
+            alert('You need to enter values to the box!');
+          }
+          break;
+        }
+      case 'both':
+        {
+          allSites = customLinks ? defaultSites.map(site => site.url).concat(customLinks) : defaultSites.map(site => site.url);
+          break;
+        }
+      default:
+        {
+          allSites = defaultSites.map(site => site.url);
+          break;
+        }
     }
 
     for (const site of allSites) {
@@ -70,9 +69,28 @@ const openDefaultLinks = () => {
   });
 };
 
+const setInsertCustomClinks = () => {
+  const testButton = document.querySelector('button#insert-custom-links');
+
+  testButton.addEventListener('click', () => {
+
+    chrome.tabs.query({
+      currentWindow: true,
+      active: false
+    }, tabsArray => {
+      const customLinks = tabsArray.filter(tab => !defaultSites.find(site => site.url === tab.url));
+      document.querySelector('#custom-links-textarea').value = `${customLinks.map(tab=>tab.url)}`;
+    });
+  });
+}
+
 // run the code when DOM is fully loaded.
 if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
-  openDefaultLinks();
+  setOpenDefaultLinks();
+  setInsertCustomClinks();
 } else {
-  document.addEventListener('DOMContentLoaded', openDefaultLinks);
+  document.addEventListener('DOMContentLoaded', () => {
+    setOpenDefaultLinks();
+    setInsertCustomClinks();
+  });
 }
